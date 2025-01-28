1f826f46d1c4ef2fb6ad1e135ddb16bfbc59e1d2
hoei-yoshimura <syoshimura@hoei-tex.co.jp>
2025-01-28 16:43:26 +0900

    レスポンシブに表のサイズを合わせた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-
 import AboutMe from "@/components/AboutMe";
 import Works from "@/components/Works";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full max-w-screen-lg mx-auto px-4">
       <AboutMe />
       <Works />
     </div>
