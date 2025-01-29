384fcc0f295a0a8c9b159b1d49bf9496f1048ef1
hoei-yoshimura <syoshimura@hoei-tex.co.jp>
2025-01-29 18:15:26 +0900

    FVっぽいのの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Works from "@/components/Works";
 export default function Home() {
   return (
     <div className="w-full max-w-screen-lg mx-auto px-4">
+      <div className="flex justify-center items-center h-full p-6 text-2xl md:text-4xl font-extrabold tracking-wide text-white bg-black border border-gray-800 rounded-xl shadow-md hover:bg-gray-900 transition-all duration-300">
+        <br/>IT×〇〇をより身近に<br/>
+      </div>
       <AboutMe />
       <Works />
     </div>
